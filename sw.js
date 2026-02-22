e1dc10711cd24dcdab7845f011d09f31aa85b5ea
sachitchaturvedi9-cmd <sachitchaturvedi9@gmail.com>
2026-02-22 10:11:28 +0530

    Update service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,20 +1,43 @@
-const CACHE = 'godcalc-v1';
-const FILES = [
-  '/',
-  '/index.html',
-  '/tools.html',
-  '/style.css',
-  '/Script.js',
-];
+const CACHE = 'godcalc-v3';
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './tools.html',
+        './style.css',
+        './Script.js',
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
   );
 });
